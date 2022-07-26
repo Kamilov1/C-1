52f6ea9d3e8116eadeb440b0a23379c2aa1dcbc1
Kamilov1 <itprofessiya1@gmail.com>
2022-07-26 20:04:21 +0300

    2 проблемы не могу понять

--- Examples005_IfElse/Program.cs
+++ Examples005_IfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
