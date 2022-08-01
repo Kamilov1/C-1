31ac95e914041a0ac6ac5003b4a97c98498ccd3a
Kamilov1 <itprofessiya1@gmail.com>
2022-08-01 17:07:21 +0300

    This is examples from second lecture

---
+++ Examples008_Leksiya2/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 11;
+int b1 = 45;
+int c1 = 77;
+int a2 = 98;
+int b2 = 24;
+int c2 = 25;
+int a3 = 12;
+int b3 = 323;
+int c3 = 34;
+
+/*int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);*/
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
+
+Console.WriteLine(max);
