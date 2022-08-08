4f1c72c7eb2d9fa27967f6f151e7a29f51732c88
Kamilov1 <itprofessiya1@gmail.com>
2022-08-08 16:34:19 +0300

    мДа Сложновато эти языки програмирования

---
+++ Examples012_Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿//Вид 1
+void Method()
+{
+    Console.WriteLine("Автор ...");
+}
+//Methods();
+//Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения");
+//Вид 2.1
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Текст", count: 4);
+//Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+//Вид 4
+/*string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result += text;
+        i++;
+    }
+    return result;
+}
+string result = Method4(10, "z");
+Console.WriteLine(result);*/
+//Вид for
+string Method5(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
+string result = Method5(10, "z");
+Console.WriteLine(result);
---
+++ Examples013_Methods2/Program.cs
@@ -0,0 +1,39 @@
+﻿// FOR
+/*for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}*/
+
+int[] arr = {1, 2, 5, 7, 9, 3, 5, 6, 4, 8};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
